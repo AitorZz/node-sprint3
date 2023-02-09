187d03fdabebfe237b5e0633ed0948b5cb9d1de3
Aitor Zamora <you@example.com>
2023-02-09 11:27:39 +0100

    revisió oriol

--- Entrega_3.1/Callback_hell_Reverse.js
+++ Entrega_3.1/Callback_hell_Reverse.js
@@ -1,3 +1,5 @@
+// Revisat per el Oriol
+
 /*Entrega 3.1 El codi adjunt llegeix un fitxer situat en un directori inbox i
   escriu el seu contingut invertit en un altre fitxer al directori outbox.
    Reestructura i simplifiqui el codi existent per a evitar el denominat Callback Hell*/
@@ -14,9 +16,9 @@ async function modifydir() {
   try {
     const files = await readdir(inbox);
     for (const file of files) {
-      const readFile = await readFile(join(inbox, file), "utf8");
+      const textFile = await readFile(join(inbox, file), "utf8");
       await writeFile(join(outbox, file), reverseText(textFile));
-      console.log(`Received ${readFile}`);
+      console.log(`Received ${textFile}`);
     }
   } catch (err) {
     console.log(err);
